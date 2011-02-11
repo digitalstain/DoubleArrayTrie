b5f4eb3cec4471831f828828cb5ec796cf554aab
Chris Gioran <chris.gioran@gmail.com>
2011-02-11 10:46:24 +0200

    Minor formatting and additions to .gitignore

--- src/test/org/digitalstain/datrie/TestDoubleArrayTrie.java
+++ src/test/org/digitalstain/datrie/TestDoubleArrayTrie.java
@@ -30,7 +30,7 @@ import org.digitalstain.datrie.store.IntegerList;
 public class TestDoubleArrayTrie extends TestCase {
 
 	public void testRobustnessUnderStress() {
-		
+
 		final int ALPHABET_SIZE = 120;
 		final int NUMBER_OF_STRINGS = 100000;
 		final int MAXIMUM_STRING_SIZE = 100;
@@ -39,27 +39,27 @@ public class TestDoubleArrayTrie extends TestCase {
 		AbstractDoubleArrayTrie trie = new DoubleArrayTrieImpl(ALPHABET_SIZE);
 		Random rng = new Random();
 
-		for(int i = 0; i < NUMBER_OF_STRINGS; i++) {
+		for (int i = 0; i < NUMBER_OF_STRINGS; i++) {
 			IntegerList toAdd = new IntegerArrayList(MAXIMUM_STRING_SIZE);
-			for(int j = 0; j < MAXIMUM_STRING_SIZE; j++) {
+			for (int j = 0; j < MAXIMUM_STRING_SIZE; j++) {
 				toAdd.add(rng.nextInt(ALPHABET_SIZE));
 			}
 			data.add(toAdd);
 		}
 
-		for(IntegerList list : data) {
+		for (IntegerList list : data) {
 			assertTrue(trie.addToTrie(list));
 			assertFalse(trie.addToTrie(list));
 		}
 
-		for(IntegerList list : data) {
+		for (IntegerList list : data) {
 			assertEquals(SearchResult.PERFECT_MATCH, trie.containsPrefix(list));
 		}
 
-		for(IntegerList list : data) {
+		for (IntegerList list : data) {
 			int removeSize = rng.nextInt(list.size()) + 1;
-			for(; removeSize > 0; removeSize--) {
-				list.remove(list.size()-1);
+			for (; removeSize > 0; removeSize--) {
+				list.remove(list.size() - 1);
 			}
 			assertEquals(SearchResult.PURE_PREFIX, trie.containsPrefix(list));
 		}
@@ -71,7 +71,7 @@ public class TestDoubleArrayTrie extends TestCase {
 
 	public void testMarginCases() {
 		AbstractDoubleArrayTrie trie = new DoubleArrayTrieImpl(3);
-		
+
 		IntegerList empty = new IntegerArrayList();
 		assertEquals(SearchResult.PURE_PREFIX, trie.containsPrefix(empty));
 
@@ -91,7 +91,7 @@ public class TestDoubleArrayTrie extends TestCase {
 
 		assertEquals(SearchResult.PERFECT_MATCH, trie.containsPrefix(one));
 		assertEquals(SearchResult.PURE_PREFIX, trie.containsPrefix(empty));
-		
+
 		one.add(1);
 		assertEquals(SearchResult.NOT_FOUND, trie.containsPrefix(one));
 	}
@@ -115,7 +115,7 @@ public class TestDoubleArrayTrie extends TestCase {
 		assertEquals(3, trie.getSearchCountFor(string1));
 		assertEquals(2, trie.getSearchCountFor(string2));
 		assertEquals(3, trie.getSearchCountFor(string2));
-		
+
 		IntegerList string3 = new IntegerArrayList();
 		string3.add(1);
 		string3.add(2);
